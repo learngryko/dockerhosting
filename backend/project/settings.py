5bf4e95d63e5d525f3a9d1854fae20cc88177561
theripson <mat0007@wp.pl>
2024-10-12 17:00:43 +0200

    django app createion

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1r*5y8lfqbenyg&#r^l8r_9-#sl9+ax#sq5u&@_(hnnma2%w%7'
+SECRET_KEY = 'django-insecure-(@*w8#58lupsczjqt%syf&j4-(48-z7*89dgxvx#%%vmm7&+o*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
