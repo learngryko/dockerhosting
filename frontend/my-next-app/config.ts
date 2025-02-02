c33da2c56ec5ce0cc00c088c6077904f76b066be
mateusz <mat0007@wp.pl>
2025-02-02 21:11:39 +0100

    debug

--- frontend/my-next-app/config.ts
+++ frontend/my-next-app/config.ts
@@ -1,10 +1,9 @@
 const settings = {
-  backendurl: process.env.API_URL,
-  host_ip: process.env.HOST_IP
+  backendurl: process.env.NEXT_PUBLIC_API_URL,
+  host_ip: process.env.NEXT_PUBLIC_HOST_IP
 };
 
-
-console.log(settings.backendurl)
-console.log(settings.host_ip)
+console.log(settings.backendurl);
+console.log(settings.host_ip);
 
 export default settings;
